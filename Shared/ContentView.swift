2867ecc86d2d63bc58834b0349b03d42513ee671
anchor <kyaputen1028@gmail.com>
2021-02-28 18:16:47 +0900

    Editor

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,17 +6,40 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
     var body: some View {
-        Text("hello word")
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Spacer()
+            Text("SwiftUI for iOS 14")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.white)
+            Text("20 Sections")
+                .font(.footnote)
+                .foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
     
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        Group {
+            ContentView()
+            ContentView()
+                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/))
+        }
     }
 }
